56d9995280e07a9769d2c6af235e474c9a4d0f27
Vargentum <vargentum@gmail.com>
2015-08-04 18:29:38 +0300

    add classNames utility

--- src/js/PassMeter.js
+++ src/js/PassMeter.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Bootstrap from "react-bootstrap";
 import {Col} from "react-bootstrap";
+import classNames from "classNames";
+
+
 
 export default class PassMeter extends React.Component {
   constructor(args) {
@@ -8,8 +11,16 @@ export default class PassMeter extends React.Component {
   }
 
   render() {
+    let cls = classNames(
+      'foo',
+      {
+        bar: true
+      }
+    )
+
     return (
       <div>
+        {cls}
         <Col xs={12}>1</Col>
         <Col xs={6}>2</Col>
       </div>
